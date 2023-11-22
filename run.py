c034d65fa4d636391b6e036498037f737bdcf7c6
osaroo3 <>
2023-11-22 04:12:27 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -17,7 +17,7 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    while True:    
+    while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
@@ -29,7 +29,7 @@ def get_sales_data():
         if validate_data(sales_data):
             print("Data is valid!")
             break
-        
+
     return sales_data
 
 
@@ -50,4 +50,17 @@ def validate_data(values):
         return False
     return True
 
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
